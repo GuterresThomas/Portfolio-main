c4e6bfee0a2a815c332a6976950bc2224e4aad5c
Guterresthomas <guterresmikael@gmail.com>
2023-07-10 16:20:06 -0300

    aa

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio
+
+https://guterresthomas.github.io/Portfolio/
+
+![portifolio](https://github.com/GuterresThomas/Portfolio/assets/131294494/dacc8f99-b125-4d86-9f3c-b25c8543e988)
+
---
+++ script.js
@@ -0,0 +1,30 @@
+const pokedex = document.getElementById('pokeDex');
+const blog = document.getElementById('blog');
+const loginPage = document.getElementById('loginPage');
+const gitHub = document.getElementById('gitHub');
+const whatsBtn = document.getElementById('whatsBtn');
+const otica = document.getElementById('otica');
+
+pokeDex.addEventListener('click', function() {
+    window.open('https://guterresthomas.github.io/PokeDex/', '_blank');
+});
+
+blog.addEventListener('click', function() {
+    window.open(' https://guterresthomas.github.io/BlogResp/', '_blank');
+});
+
+loginPage.addEventListener('click', function() {
+    window.open('https://tainagrendene.com.br/AutoMakeByTaina/', '_blank');
+});
+
+gitHub.addEventListener('click', function() {
+    window.open('https://github.com/GuterresThomas', '_blank');
+});
+
+whatsBtn.addEventListener('click', function() {
+    window.open('https://wa.me/5555997004539', '_blank');
+});
+
+otica.addEventListener('click', function() {
+    window.open('https://otica.onrender.com', '_blank');
+});
